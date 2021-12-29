f9546b68d8d157140e35ccc703ec523818d35bed
Sam Oum <oumsamon@gmail.com>
2021-12-28 22:49:27 -0600

    Update challenge.py
    Solved

--- challenge.py
+++ challenge.py
@@ -1,28 +1,67 @@
 ######################################################
 # Part 1: Most Clocks are Normal, But Some are Cuckoo
 
-time = 8
+# time = input('What time is it?: ')
+# # num = 12
+# def check_time(num):
+#     num = int(num)
+#     if num < 9:
+#         print('Morning is wonderful. Its only drawback is that it comes at such an inconvenient time of day.')
+#     elif num <= 16:
+#         print('Working hard or hardly working?')
+#     elif num <= 20:
+#         print('How did it get so late so soon?')
+#     elif num < 22:
+#         print('A day without sunshine is like, you know, night.')
+#     else:
+#         print('Burning the midnight oil!')
+
+# check_time(time)
+
+
 # Complete the prompt here
 
 
 ######################################################
 # Part 2: I Came, I 'Saur, I Conquered
 
-angry = True
-bored = True
-hungry = False
-tired = False
+# angry = False
+# bored = False
+# hungry = False
+# tired = False
 
+# def trex_mood(angry, bored, hungry, tired):
+#     if angry and hungry and bored:
+#         # action
+#         print('Eat Triceratops')
+#     elif tired and hungry:
+#         print('Eat Iguanadon')
+#     elif hungry and bored:
+#         print('Eat Stegasaurus')
+#     elif tired:
+#         print('Im going to sleep')
+#     elif angry and bored:
+#         print('Going to fight a Velociraptor')
+#     elif angry or bored:
+#         print('Rawr!!!')
+#     else:
+#         print('Smile toothy to you')
+
+# trex_mood(angry, bored, hungry, tired)
 # Complete the prompt here
 # Example `if` statement:
-if bored:
-    print('T-Rex roars! RAWR!')
+# if bored:
+#     print('T-Rex roars! RAWR!')
 
 ######################################################
 # Part 3: IOU
 
 disney_characters = ['simba', 'ariel', 'pumba',
                      'flounder', 'nala', 'ursula', 'scar', 'flotsam', 'timon']
+
+def disney_finding(disney_characters):
+    for i in disney_characters:
+        
 
 # Complete the prompt here
 
